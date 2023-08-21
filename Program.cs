679b5ee7f22301dbdd49d4f0201a05eab31dae79
mustafaeren <mstafa.erenn@gmail.com>
2023-08-22 01:01:30 +0300

    Create project files and docker-compose

---
+++ Program.cs
@@ -0,0 +1,51 @@
+
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(ConfigureElasticSink(builder.Configuration))
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration)
+{
+    var elasticUri = configuration["ElasticConfiguration:Uri"];
+    var indexFormat = "test-{0:yyyy.MM.dd}";
+
+    return new ElasticsearchSinkOptions(new Uri(elasticUri))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = indexFormat,
+        NumberOfReplicas = 1,
+        NumberOfShards = 2
+    };
+}
